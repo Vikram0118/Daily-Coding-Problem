cecfb2d753816caf545473f299fb144464ea22d9
Vikram0118 <vikramgit001@gmail.com>
2023-02-11 23:32:31 +0530

    linkedlist started

--- Day20Easy.java
+++ Day20Easy.java
@@ -1,5 +1,63 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d ){
+        data = d;
+        next = null;
+    }
+}
+
+class Linked {
+    public Node insertNode(int data, Node root) {
+        if(root == null) {
+            return createNode(data);
+        } else {
+            Node firstNode = root;
+            Node temp = createNode(data);
+            while(root.next != null){
+                root = root.next;
+            }
+            root.next = temp;
+            temp.next = null;
+            return firstNode;
+        }
+    }
+
+    private Node createNode(int data) {
+        Node a = new Node(data);
+        return a;
+    }
+
+    public void printList(Node root) {
+        while(root != null) {
+            System.out.print(root.data+" ");
+            root = root.next;
+        }
+    }
+}
+
 public class Day20Easy {
     public static void main(String[] args) {
-        
+        Node root1  = null;
+        Node root2  = null;
+        Linked linked = new Linked();
+
+        root1 = linked.insertNode(3, root1);
+        root1 = linked.insertNode(7, root1);
+        root1 = linked.insertNode(8, root1);
+        root1 = linked.insertNode(10, root1);
+        root2 = linked.insertNode(99, root2);
+        root2 = linked.insertNode(1, root2);
+        root2 = linked.insertNode(8, root2);
+        root2 = linked.insertNode(10, root2);
+
+        System.out.println("LIST 1 : ");
+        linked.printList(root1);
+        System.out.println();
+        System.out.println("LIST 2 : ");
+        linked.printList(root2);
+        System.out.println();
+        System.out.println("INTERSECTING POINT : ");
     }
 }
---
+++ Day21Easy.java
@@ -0,0 +1,30 @@
+// hard
+
+class Day21Easy{
+    public static void main(String[] args) {
+        int a[][] = {{30, 70},
+                     {0, 50},
+                     {60, 150}
+                    };
+        // int a[][] = {{30, 50},
+        //              {20, 160},
+        //              {0, 150},
+        //              {60, 150}
+        //             };
+        int n =1;
+
+        // for(int i=0; i<a.length; i++) {
+            for(int j=1; j<a.length; j++) {
+                if( ( (a[0][0]>a[j][0]) && (a[0][1]>a[j][1]) && (a[j][1]>a[0][0]) ) || 
+                    ( (a[0][0]<a[j][0]) && (a[j][0]<a[0][1]) && (a[j][1]>a[0][1]) ) || 
+                    ( (a[0][0]<a[j][0]) && (a[0][1]>a[j][1]) ) ||
+                    ( (a[0][0]>a[j][0]) && (a[0][1]<a[j][1]) ) ) {
+                    n++;
+                    System.out.println("j = "+j);
+                }
+            }
+        // }
+
+        System.out.println(n);
+    }
+}
