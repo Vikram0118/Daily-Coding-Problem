4da09b7fd3ce14d46411a2d6439cab9c8cfb8b88
Vikram0118 <vikramgit001@gmail.com>
2023-02-12 00:10:29 +0530

    graph and tree theriyama onnum panna mudiyala

--- Day22Medium.java
+++ Day22Medium.java
@@ -1,5 +1,35 @@
+
+// This problem was asked by Microsoft.
+
+// Given a dictionary of words and a string made up of those words (no spaces), 
+// return the original sentence in a list. If there is more than one possible reconstruction,
+//  return any of them. If there is no possible reconstruction, then return null.
+
+// For example, given the set of words 'quick', 'brown', 'the', 'fox', and 
+// the string "thequickbrownfox", you should return ['the', 'quick', 'brown', 'fox'].
+
+// Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and 
+// the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day22Medium {
     public static void main(String[] args) {
-        
+        // String s1="quick", s2="brown", s3="the", s4="fox";  
+        Map<Integer, String> dictionary = new HashMap<>();
+
+        dictionary.put(1, "quick");
+        dictionary.put(2, "brown");
+        dictionary.put(3, "the");
+        dictionary.put(4, "fox");
+        String sentence = "thequickbrownfox";
+
+        System.out.println(dictionary);
+
+
+
+
+
     }
 }
